fdec7acdb4f4b03abd1a19ef07110a1e0d9857f2
Rayden X <102635182+Rayden-Xu@users.noreply.github.com>
2023-03-02 17:30:40 -0500

    Update CLV_prediction.py

--- pages/CLV_prediction.py
+++ pages/CLV_prediction.py
@@ -1,6 +1,34 @@
 import pandas as pd
 import streamlit as st
 
+
+
+st.set_page_config(page_title= "CLV Predication")
+
+st.image(
+    "Food.jpg",
+    use_column_width = True,
+)
+
+st.title("CLV Predication")
+
+st.write("")
+
+
+with st.expander("Attribute/Filter"):
+  
+
+  st.number_input(
+    'C_BIRTH_YEAR:',
+    0)
+  st.number_input(
+    'CA_ZIP:',
+    0)
+  st.number_input(
+    'CD_DEP_COUNT:',
+    0)
+    
+with st.expander("About this app"):
 st.write('## XGBoost model to predict CLV')
 
 st.write('Type inputs here......')
