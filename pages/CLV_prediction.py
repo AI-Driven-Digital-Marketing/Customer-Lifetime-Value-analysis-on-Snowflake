1891f0a1dba54a03134f2c93347095865beb16d3
Rayden X <102635182+Rayden-Xu@users.noreply.github.com>
2023-03-02 17:33:33 -0500

    Update CLV_prediction.py

--- pages/CLV_prediction.py
+++ pages/CLV_prediction.py
@@ -1,30 +1,22 @@
 import pandas as pd
 import streamlit as st
 
+# st.set_page_config(page_title= "CLV Predication")
 
-
-st.set_page_config(page_title= "CLV Predication")
-
-st.image(
-    "Food.jpg",
-    use_column_width = True,
-)
-
+# st.image(
+#     "Food.jpg",
+#     use_column_width = True,
+# )
 st.title("CLV Predication")
 
-st.write("")
-
-
 with st.expander("Attribute/Filter"):
-  
-
-  st.number_input(
+    st.number_input(
     'C_BIRTH_YEAR:',
     0)
-  st.number_input(
+    st.number_input(
     'CA_ZIP:',
     0)
-  st.number_input(
+    st.number_input(
     'CD_DEP_COUNT:',
     0)
     
