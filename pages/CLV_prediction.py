a76d9f0591ab9b5cb74daff9f35e7a8b370963d9
Rayden X <102635182+Rayden-Xu@users.noreply.github.com>
2023-03-02 17:48:45 -0500

    Update CLV_prediction.py

--- pages/CLV_prediction.py
+++ pages/CLV_prediction.py
@@ -10,25 +10,29 @@ import streamlit as st
 st.title("XGBoost model to predict CLV")
 
 with st.expander("Attribute/Filter"):
-    st.number_input(
-    'C_BIRTH_YEAR:',
-    0)
-    st.number_input(
-    'CA_ZIP:',
-    0)
-    st.number_input(
-    'CD_DEP_COUNT:',
-    0)
-#Categorical
-    st.selectbox(
-      'CD_Gender',
-      ('Option1',
-       'Option2',
-       'Option3C_BIRTH_YEAR',
-       'CD_GENDER', 
-       'CD_MARITAL_STATUS', 
-       'CD_CREDIT_RATING', 
-       'CD_EDUCATION_STATUS', 
-       'CD_DEP_COUNT'),
-     )
+    col1, col2 = st.columns(2,gap = "medium")
+    with col1:
+        st.markdown('#### Numeric Feature')
+        st.number_input(
+        'C_BIRTH_YEAR:',
+        0)
+        st.number_input(
+        'CA_ZIP:',
+        0)
+        st.number_input(
+        'CD_DEP_COUNT:',
+        0)
+    with col2:
+        st.markdown('### Categorical ')
+        st.selectbox(
+          'CD_Gender',
+          ('Option1',
+           'Option2',
+           'Option3',
+           'CD_GENDER', 
+           'CD_MARITAL_STATUS', 
+           'CD_CREDIT_RATING', 
+           'CD_EDUCATION_STATUS', 
+           'CD_DEP_COUNT'),
+         )
 
