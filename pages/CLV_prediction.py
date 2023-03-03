bc610aece23ac035d88d29973618316d5106e8eb
Rayden X <102635182+Rayden-Xu@users.noreply.github.com>
2023-03-02 21:26:09 -0500

    Update CLV_prediction.py

--- pages/CLV_prediction.py
+++ pages/CLV_prediction.py
@@ -1,12 +1,15 @@
 import pandas as pd
 import streamlit as st
+from streamlit.report_thread import get_report_ctx
+from streamlit.server.server import Server
 
-# st.set_page_config(page_title= "CLV Predication")
+st.set_page_config(page_title= "Customer Lifetime Value")
 
-# st.image(
-#     "Food.jpg",
-#     use_column_width = True,
-# )
+st.image(
+    "src/1649251328-maximize-your-clv.webp",
+    use_column_width = True,
+)
+
 st.title("XGBoost model to predict CLV")
 
 with st.expander("Attribute/Filter"):
