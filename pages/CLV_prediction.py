5739a691178edbbc5a057954eb046e08a6e0221e
Rayden X <102635182+Rayden-Xu@users.noreply.github.com>
2023-03-02 21:27:01 -0500

    Update CLV_prediction.py

--- pages/CLV_prediction.py
+++ pages/CLV_prediction.py
@@ -3,7 +3,7 @@ import streamlit as st
 from streamlit.report_thread import get_report_ctx
 from streamlit.server.server import Server
 
-st.set_page_config(page_title= "Customer Lifetime Value")
+# st.set_page_config(page_title= "Customer Lifetime Value")
 
 # st.image(
 #     "src/1649251328-maximize-your-clv.webp",
