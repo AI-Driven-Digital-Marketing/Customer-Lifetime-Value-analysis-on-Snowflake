65e47cb00df7161c460cfc73fbf1c81bbb8beb31
Rayden X <102635182+Rayden-Xu@users.noreply.github.com>
2023-03-05 21:12:12 -0500

    Update CLV_prediction.py

--- pages/CLV_prediction.py
+++ pages/CLV_prediction.py
@@ -20,9 +20,8 @@ with st.expander("Attribute/Filter"):
         'C_BIRTH_YEAR:', min_value=1924,
         max_value=2020)
         
-        
         json_dict=json.load(open('zip_json.json'))
-        option = list(json_dict.items())
+        options = list(json_dict.items())
         st.selectbox('CA_ZIP', options, index=0, format_func=lambda x: x.title(), typeahead=True)
         
     with col2:
