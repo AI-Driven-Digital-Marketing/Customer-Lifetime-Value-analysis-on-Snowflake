5cf4ff0dbd92094ebb274e0a9b91a83041bb7efb
Smart Husky <64514218+DDuan-zw@users.noreply.github.com>
2023-03-06 15:10:01 -0500

    update streamlit

--- pages/CLV_prediction.py
+++ pages/CLV_prediction.py
@@ -1,8 +1,18 @@
 import pandas as pd
 import streamlit as st
 import json
+import numpy as np
+import sys
+import cachetools
+import joblib
 # from streamlit.report_thread import get_report_ctx
 # from streamlit.server.server import Server
+import snowflake.snowpark
+from snowflake.snowpark import functions as F
+from snowflake.snowpark.session import Session
+from snowflake.snowpark.types import PandasDataFrame,PandasSeries
+from snowflake.snowpark.functions import udf, sum, col,array_construct,month,year,call_udf,lit,count
+from snowflake.snowpark.version import VERSION
 
 st.set_page_config(page_title= "Customer Lifetime Value")
 
@@ -19,12 +29,14 @@ with col1:
     Cus_by = st.number_input('Customer Birth Year:', 
                     min_value=1924,
                     max_value=2020, 
-                    help = 'Please type VALID birth Year!!(Range: 1924~2020)')
+                    help = 'Please type VALID birth Year!!(Range: 1924~2020)'
+                            )
 
     Cs_zip = st.number_input( 'Customer Zip Code:', 
                     min_value= 601, 
-                    max_value= 99981, 
-                    #help = 'Please type VALID zip code!'
+                    max_value= 99981,
+                    value  = 66668,
+                    step = 1
                             )
 
 
@@ -48,36 +60,92 @@ with col2:
     Cus_dep = st.selectbox( 'CD_DEP_COUNT',
                  ['0', '1'],
       )
-         
-#Output
 
-if st.button('Submit'):
-    st.button('Reset')
-    col1, col2 = st.columns(2,gap = "medium")
-    with col1:
-        st.markdown('''
-        --------------------------------
-        ## Customer Lifetime Value:
 
-        ''')
-    with col2:    
-        st.markdown('''
+
+col3, col4 = st.columns([8,2],gap = "medium")
+
+with col3:
+    model_select = st.radio('Select the Model here:',
+                           ['Linear Regression', 'XGBoost'])
+with col4:
+    submit =  st.button('Submit')
+    reset = st.button('Reset ')
+@st.cache_resource
+def initialize_SF():
+    connection_parameters = json.load(open('connection.json'))
+    session = Session.builder.configs(connection_parameters).create()
+    session.sql_simplifier_enabled = True
+    ca_zip = json.load(open('src/zip_json.json'))    
+    # set up feature engineering/inference warehouse
+    session.use_warehouse('FE_AND_INFERENCE_WH')
+    session.use_database('tpcds_xgboost')
+    session.use_schema('demo')
+    session.add_packages('snowflake-snowpark-python', 'scikit-learn', 'pandas', 'numpy', 'joblib', 'cachetools', 'xgboost', 'joblib')
+    session.add_import("@ml_models_10T/model.joblib")  
+    return session, ca_zip
+session, ca_zip = initialize_SF()
+
+# choose model here
+if model_select == 'XGBoost':    
+    model_name = 'ml_models_10T'
+else:
+    model_name = 'ml_models_LR_10T'
+
+
+features = [ 'C_BIRTH_YEAR', 'CA_ZIP', 'CD_GENDER', 'CD_MARITAL_STATUS', 'CD_CREDIT_RATING', 'CD_EDUCATION_STATUS', 'CD_DEP_COUNT']
+
+@cachetools.cached(cache={})
+def read_file(filename):
+    import os, joblib
+    import_dir = sys._xoptions.get("snowflake_import_directory")
+    if import_dir:
+        with open(os.path.join(import_dir, filename), 'rb') as file:
+            m = joblib.load(file)
+            return m
+
+@F.pandas_udf(session=session, max_batch_size=10000, is_permanent=True, stage_location=f'@{model_name}', replace=True, name="clv_xgboost_udf")
+def predict(df:  PandasDataFrame[int, str, str, str, str, str, int]) -> PandasSeries[float]:
+    m = read_file('model.joblib')       
+    df.columns = features
+    return m.predict(df)   
+
+if submit:
+    typed_input = [[Cus_by, ca_zip.get(str(Cs_zip), '66668'),Cus_gender,Cus_marital,Cus_credit,Cus_edu, int(Cus_dep)]]
+    input_df = session.create_dataframe(typed_input, schema=features)
+    typed_output = input_df.select(*input_df,
+                    predict(*input_df).alias('PREDICTION'))
+    typed_output.show()
+if reset:
+    st.write('xuyao reset ma? hao ma fan')
+    
+# if st.button('Submit'):
+#     st.button('Reset')
+#     col1, col2 = st.columns(2,gap = "medium")
+#     with col1:
+#         st.markdown('''
+#         --------------------------------
+#         ## Customer Lifetime Value:
+
+#         ''')
+#     with col2:    
+#         st.markdown('''
         
-        *<Modeling result ouput on here>*
-        ''')
+#         *<Modeling result ouput on here>*
+#         ''')
        
     
     
-elif st.button('Reset'):
+# elif st.button('Reset'):
    
-     st.write('<reset explander value>')
+#      st.write('<reset explander value>')
     
-else:
-     st.markdown(
-         '''
-         ----------  
-         ## Customer Lifetime Value:
-         Please setup filter value！!
-         ''')
+# else:
+#      st.markdown(
+#          '''
+#          ----------  
+#          ## Customer Lifetime Value:
+#          Please setup filter value！!
+#          ''')
          
     
