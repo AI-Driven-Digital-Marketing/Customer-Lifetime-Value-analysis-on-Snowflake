d22c318be00afe47b0b1171921f23d7e1d4c1760
Rayden X <102635182+Rayden-Xu@users.noreply.github.com>
2023-03-05 23:27:06 -0500

    Update CLV_prediction.py

--- pages/CLV_prediction.py
+++ pages/CLV_prediction.py
@@ -24,7 +24,7 @@ with st.expander("Attribute/Filter"):
         max_value=2020)
         
         st.number_input(
-        'C_ZIP:', 0)
+        'C_ZIP:', min_value=601, max_value= 99981)
   
         
     with col2:
