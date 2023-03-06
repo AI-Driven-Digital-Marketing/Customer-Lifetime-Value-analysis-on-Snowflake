b3f25feb686cce841b67538174b5093bfad6873c
Smart Husky <64514218+DDuan-zw@users.noreply.github.com>
2023-03-06 14:10:40 -0500

    updates

--- pages/CLV_prediction.py
+++ pages/CLV_prediction.py
@@ -11,60 +11,43 @@ st.image(
     width = 600,
 )
 
-st.title("XGBoost model to predict CLV")
+#st.title("XGBoost model to predict CLV")
 
-with st.expander("Attribute/Filter"):
+col1, col2 = st.columns(2,gap = "medium")
+with col1:
+    st.markdown('#### Numeric Features')
+    Cus_by = st.number_input('Customer Birth Year:', 
+                    min_value=1924,
+                    max_value=2020, 
+                    help = 'Please type VALID birth Year!!(Range: 1924~2020)')
 
-    
-    col1, col2 = st.columns(2,gap = "medium")
-    with col1:
-        st.markdown('#### Numeric Features')
-        st.number_input(
-        'C_BIRTH_YEAR:', min_value=1924,
-        max_value=2020, help = 'Please type VALID birth Year!!(Range: 1924~2020)')
-        
-        st.number_input(
-        'C_ZIP:', min_value= 601, max_value= 99981, help = 'Please type VALID zip code!!(eg: 02115: Boston) ')
-  
-        
-    with col2:
-        st.markdown('#### Categorical Features ')
-        st.selectbox(
-          'CD_Gender',
-          ('M',
-           'F'), help= 'M: Male, F: Female'
-         )
-         
-        st.selectbox(
-          'CD_MARITAL_STATUS',
-          ('S',
-           'D',
-           'W',
-           'U', 
-           'M'),
-         )
-        st.selectbox(
-          'CD_CREDIT_RATING',
-          ('Low Risk',
-           'Unknown',
-           'Good',
-           'High Risk'),
-           )
-        
-        st.selectbox(
-          'CD_EDUCATION_STATUS',
-          ('Advanced Degree',
-           'Secondary',
-           '2 yr Degree',
-           '4 yr Degree', 
-           'Unknown', 
-           'Primary', 
-           'College'),)
-        st.selectbox(
-          'CD_DEP_COUNT',
-          ('0',
-           '1'),
-          )
+    Cs_zip = st.number_input( 'Customer Zip Code:', 
+                    min_value= 601, 
+                    max_value= 99981, 
+                    #help = 'Please type VALID zip code!'
+                            )
+
+
+with col2:
+    st.markdown('#### Categorical Features ')
+    Cus_gender = st.selectbox('CD_Gender',
+                              ['M', 'F'], 
+                              help= 'M: Male, F: Female'
+     )
+
+    Cus_marital = st.selectbox( 'CD_MARITAL_STATUS',
+                 ['S','D','W', 'U', 'M'],
+     )
+    Cus_credit = st.selectbox('CD_CREDIT_RATING',
+                 ['Low Risk','Unknown','Good','High Risk']
+       )
+
+    Cus_edu = st.selectbox( 'CD_EDUCATION_STATUS',
+                 ['Advanced Degree','Secondary','2 yr Degree','4 yr Degree','Unknown','Primary','College']
+                )
+    Cus_dep = st.selectbox( 'CD_DEP_COUNT',
+                 ['0', '1'],
+      )
          
 #Output
 
@@ -98,4 +81,3 @@ else:
          ''')
          
     
-
