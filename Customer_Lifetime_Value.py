0a516ddec730afda5798f2372bd9580c6339070f
Rayden X <102635182+Rayden-Xu@users.noreply.github.com>
2023-03-12 19:31:43 -0400

    Update Customer_Lifetime_Value.py

--- Customer_Lifetime_Value.py
+++ Customer_Lifetime_Value.py
@@ -11,45 +11,47 @@ st.image(
 
 col1, col2 = st.columns(2,gap = "medium")
 with col1:
-    st.radio('Select the Model here:',
+    Query_selection = st.radio('Select the Model here:',
                            [ 'Q1','Q2','Q3','Q4','Q5','Q6']
                             #[ 'XGBoost']
                            )
 
 
 with col2:
-    st.markdown('#### Numeric Features')
-    Cus_by = st.number_input('Customer Birth Year:', 
-                    min_value=1924,
-                    max_value=2020, 
-                    help = 'Please type VALID birth Year!!(Range: 1924~2020)'
-                            )
-
-    Cs_zip = st.number_input( 'Customer Zip Code:', 
+    if Query_selection == 'Q1':
+        st.markdown('#### Numeric Features')
+        Cus_by = st.number_input('Customer Birth Year:', 
+                        min_value=1924,
+                        max_value=2020, 
+                        help = 'Please type VALID birth Year!!(Range: 1924~2020)'
+                                )
+    elif:
+         Cs_zip = st.number_input( 'Customer Zip Code:', 
                     min_value= 601, 
                     max_value= 99981,
                     value  = 66668,
                     step = 1
                             )
-    st.markdown('#### Categorical Features ')
-    Cus_gender = st.selectbox('CD_Gender',
-                              ['M', 'F'], 
-                              help= 'M: Male, F: Female'
-     )
+    else:
+        st.markdown('#### Categorical Features ')
+        Cus_gender = st.selectbox('CD_Gender',
+                                  ['M', 'F'], 
+                                  help= 'M: Male, F: Female'
+         )
 
-    Cus_marital = st.selectbox( 'CD_MARITAL_STATUS',
-                 ['S','D','W', 'U', 'M'],
-     )
-    Cus_credit = st.selectbox('CD_CREDIT_RATING',
-                 ['Low Risk','Unknown','Good','High Risk']
-       )
+        Cus_marital = st.selectbox( 'CD_MARITAL_STATUS',
+                     ['S','D','W', 'U', 'M'],
+         )
+        Cus_credit = st.selectbox('CD_CREDIT_RATING',
+                     ['Low Risk','Unknown','Good','High Risk']
+           )
 
-    Cus_edu = st.selectbox( 'CD_EDUCATION_STATUS',
-                 ['Advanced Degree','Secondary','2 yr Degree','4 yr Degree','Unknown','Primary','College']
-                )
-    Cus_dep = st.selectbox( 'CD_DEP_COUNT',
-                 ['0', '1'],
-      )
+        Cus_edu = st.selectbox( 'CD_EDUCATION_STATUS',
+                     ['Advanced Degree','Secondary','2 yr Degree','4 yr Degree','Unknown','Primary','College']
+                    )
+        Cus_dep = st.selectbox( 'CD_DEP_COUNT',
+                     ['0', '1'],
+          )
 
 
 
