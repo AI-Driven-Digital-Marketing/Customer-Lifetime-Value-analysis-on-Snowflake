5f233f500132afaae12165b4f0cb7daa7db55cc8
Rayden X <102635182+Rayden-Xu@users.noreply.github.com>
2023-03-12 16:10:55 -0400

    Update Customer_Lifetime_Value.py

--- Customer_Lifetime_Value.py
+++ Customer_Lifetime_Value.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import streamlit as st
 
-st.write('## Introduction')
+# st.write('## Introduction')
 # st.write([[1969, '66060','M','U','Low Risk','2 yr Degree', 1]])
 st.set_page_config(page_title= "Customer Lifetime Value")
 
