1b9326e4abdb3dcf7bbbba5dd0af1a3fe7c8a92f
Smart Husky <64514218+DDuan-zw@users.noreply.github.com>
2023-03-13 19:24:09 -0400

    add question 2

--- Customer_Lifetime_Value.py
+++ Customer_Lifetime_Value.py
@@ -62,8 +62,13 @@ with col1:
 
     elif Query_selection == 'Q2':
         form = st.form(key='Q2-form')
+        year_input = form.number_input('year',
+                                        min_value=1900,
+                                        max_value=2100,
+                                        value  = 2000,
+                                        help = 'Input value not in range.(Range: 1900~2100)')
         submit = form.form_submit_button('Submit')
-        pass
+        
         
     elif Query_selection == 'Q3':
         pass
@@ -119,7 +124,91 @@ with col2:
         st.write(pd.read_sql_query(q1 ,engine))
     
     elif Query_selection == 'Q2' and submit:
-        pass
+        # the query runs super slow
+        st.write(pd.read_sql_query('select * from WEB_SALES limit 10;',engine))
+        q2 = f'''WITH wscs AS
+  (SELECT sold_date_sk,
+          sales_price
+   FROM
+     (SELECT ws_sold_date_sk sold_date_sk,
+             ws_ext_sales_price sales_price
+      FROM web_sales
+      UNION ALL SELECT cs_sold_date_sk sold_date_sk,
+                       cs_ext_sales_price sales_price
+      FROM catalog_sales) sq1),
+     wswscs AS
+  (SELECT d_week_seq,
+          sum(CASE
+                  WHEN (d_day_name='Sunday') THEN sales_price
+                  ELSE NULL
+              END) sun_sales,
+          sum(CASE
+                  WHEN (d_day_name='Monday') THEN sales_price
+                  ELSE NULL
+              END) mon_sales,
+          sum(CASE
+                  WHEN (d_day_name='Tuesday') THEN sales_price
+                  ELSE NULL
+              END) tue_sales,
+          sum(CASE
+                  WHEN (d_day_name='Wednesday') THEN sales_price
+                  ELSE NULL
+              END) wed_sales,
+          sum(CASE
+                  WHEN (d_day_name='Thursday') THEN sales_price
+                  ELSE NULL
+              END) thu_sales,
+          sum(CASE
+                  WHEN (d_day_name='Friday') THEN sales_price
+                  ELSE NULL
+              END) fri_sales,
+          sum(CASE
+                  WHEN (d_day_name='Saturday') THEN sales_price
+                  ELSE NULL
+              END) sat_sales
+   FROM wscs,
+        date_dim
+   WHERE d_date_sk = sold_date_sk
+   GROUP BY d_week_seq
+   limit 1000)
+SELECT d_week_seq1,
+       round(sun_sales1/sun_sales2, 2),
+       round(mon_sales1/mon_sales2, 2),
+       round(tue_sales1/tue_sales2, 2),
+       round(wed_sales1/wed_sales2, 2),
+       round(thu_sales1/thu_sales2, 2),
+       round(fri_sales1/fri_sales2, 2),
+       round(sat_sales1/sat_sales2, 2)
+FROM
+  (SELECT wswscs.d_week_seq d_week_seq1,
+          sun_sales sun_sales1,
+          mon_sales mon_sales1,
+          tue_sales tue_sales1,
+          wed_sales wed_sales1,
+          thu_sales thu_sales1,
+          fri_sales fri_sales1,
+          sat_sales sat_sales1
+   FROM wswscs,
+        date_dim
+   WHERE date_dim.d_week_seq = wswscs.d_week_seq
+     AND d_year = {year_input}) y,
+  (SELECT wswscs.d_week_seq d_week_seq2,
+          sun_sales sun_sales2,
+          mon_sales mon_sales2,
+          tue_sales tue_sales2,
+          wed_sales wed_sales2,
+          thu_sales thu_sales2,
+          fri_sales fri_sales2,
+          sat_sales sat_sales2
+   FROM wswscs,
+        date_dim
+   WHERE date_dim.d_week_seq = wswscs.d_week_seq
+     AND d_year = {year_input}+1) z
+WHERE d_week_seq1 = d_week_seq2-53
+ORDER BY d_week_seq1
+limit 1000;
+'''
+        st.write(pd.read_sql_query(q2 ,engine))
     elif Query_selection == 'Q3' and submit:
         pass
     elif Query_selection == 'Q4' and submit:
