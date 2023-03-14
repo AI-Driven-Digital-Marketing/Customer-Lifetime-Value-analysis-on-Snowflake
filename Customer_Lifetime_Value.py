495cc438de72a5a4b8e8ff035463810511135f1c
Rayden X <102635182+Rayden-Xu@users.noreply.github.com>
2023-03-14 17:31:00 -0400

    Update Customer_Lifetime_Value.py

--- Customer_Lifetime_Value.py
+++ Customer_Lifetime_Value.py
@@ -88,13 +88,8 @@ with col1:
                                         value = 128,
                                         help = 'Input value not in range.(Range: 1~1250)')
         aggc_input = form.selectbox('AGGC ',var_json['q3_aggc'])
+        
         submit = form.form_submit_button('Submit')
-        '''
-        Qualification Substitution Variable:
-        MONTH.01=11
-        MANUFACT =128
-        AGGC = ss_ext_sales_price
-        '''
         
         
     elif Query_selection == 'Q4':
