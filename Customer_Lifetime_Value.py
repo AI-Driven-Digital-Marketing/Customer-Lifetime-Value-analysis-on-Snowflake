f3c3557061fb2d5a062fbfb293d5e92bf09850e6
Rayden X <102635182+Rayden-Xu@users.noreply.github.com>
2023-03-15 11:43:10 -0400

    Update Customer_Lifetime_Value.py

--- Customer_Lifetime_Value.py
+++ Customer_Lifetime_Value.py
@@ -89,13 +89,14 @@ with col1:
                                         value = 128,
                                         help = 'Input value should in this range.(Range: 12~998)')
         
-        aggc_input = form.selectbox('AGGC',["ss_sold_date_sk","ss_sold_time_sk","ss_item_sk","ss_customer_sk",
-                                            "ss_cdemo_sk","ss_hdemo_sk","ss_addr_sk","ss_store_sk",
-                                            "ss_promo_sk","ss_ticket_number","ss_quantity",
-                                            "ss_wholesale_cost","ss_list_price",
-                                            "ss_sales_price","ss_ext_discount_amt","ss_ext_sales_price",
-                                            "ss_ext_wholesale_cost","ss_ext_list_price","ss_ext_tax","ss_coupon_amt",
-                                            "ss_net_paid","ss_net_paid_inc_tax","ss_net_profit"],help = 'key= ss_ext_sales_price')
+        aggc_input = form.selectbox('AGGC',var_json['q3_aggc'])
+#                                     ["ss_sold_date_sk","ss_sold_time_sk","ss_item_sk","ss_customer_sk",
+#                                             "ss_cdemo_sk","ss_hdemo_sk","ss_addr_sk","ss_store_sk",
+#                                             "ss_promo_sk","ss_ticket_number","ss_quantity",
+#                                             "ss_wholesale_cost","ss_list_price",
+#                                             "ss_sales_price","ss_ext_discount_amt","ss_ext_sales_price",
+#                                             "ss_ext_wholesale_cost","ss_ext_list_price","ss_ext_tax","ss_coupon_amt",
+#                                             "ss_net_paid","ss_net_paid_inc_tax","ss_net_profit"],help = 'key= ss_ext_sales_price')
         
         submit = form.form_submit_button('Submit')
         
